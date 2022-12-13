7e3c4a05a47fe3bacd7547382132dd6ae0bb42da
axense234 <andreicomanescuonline@gmail.com>
2022-12-13 11:32:06 +0200

    Playing around with responsive images.

--- src/components/Other/Logo.tsx
+++ src/components/Other/Logo.tsx
@@ -5,8 +5,8 @@ const Logo: React.FC = () => {
   return (
     <Link to='/' className='logo'>
       <img
-        sizes='75px'
-        srcSet='https://res.cloudinary.com/birthdayreminder/image/upload/c_scale,q_80,w_75/v1669711350/CRUD%20TODO(TS%20with%20React)/Logo.png_klc5na.png, '
+        sizes='75px, 150px'
+        srcSet='https://res.cloudinary.com/birthdayreminder/image/upload/c_scale,q_80,w_75/v1669711350/CRUD%20TODO(TS%20with%20React)/Logo.png_klc5na.png, https://res.cloudinary.com/birthdayreminder/image/upload/c_scale,q_80,w_150/v1669711350/CRUD%20TODO(TS%20with%20React)/Logo.png_klc5na.png, '
         alt='Nav Logo'
       />
     </Link>
